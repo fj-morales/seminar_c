d35577aa03dd53263f98975df1db3eea483d55a8
barentroia <barentroia@gmail.com>
2018-04-04 01:53:10 +0200

    script for images

---
+++ images_report.m
@@ -0,0 +1,9 @@
+%Images produced for the report
+%bear next to women in a pool -example 1
+blending('Image Blending/source.jpg','Image Blending/target.jpg','output_example.png',1,false)
+
+%test localization destination image
+blending('Image Blending/source.jpg','Image Blending/grid.PNG','output_testLocalDestination.png',1,false)
+
+%limitation
+blending('Image Blending/rainbow.jpg','Image Blending/sky.jpeg','output_limit.png',1,false)
