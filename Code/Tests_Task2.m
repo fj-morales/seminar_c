1fda563fe3aaa67fe56bb800efd34110733eae01
Francisco Morales <fjmoralesmartinez@hotmail.com>
2018-04-05 20:51:43 +0200

    Remove unnecessary code
    Remove unnecessary code

--- Code/Tests_Task2.m
+++ Code/Tests_Task2.m
@@ -1,14 +1,11 @@
 %Test for correctness
-%Gradient Matrix
+%Gradient Matrix for irregular shape
 h = 3;
 w = 4;
 %Producing a fake image with symbols: ai where i is the pixel identifier
 fakeImage2 = sym('a',[1 h*w]);
 fakeImage2 = reshape(fakeImage2,h,w);
 selection = [0 1 0 0; 1 1 1 1; 0 1 1 0];
-% selection = ones(3,4);
-% selection = [1 1 0 0; 1 1 1 1; 0 1 1 0];
-% selection = [0 1 1 0; 1 1 1 1; 1 1 1 1; 0 1 1 0];
 U = sym('a',[h*w 1]);
 [G, g_zeros] = gradient(selection);
 g = G*U;
